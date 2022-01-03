1ad2c0dc45c25254d8dcc1d6993a96b9ee757597
Paexp <1145009208@qq.com>
2022-01-03 16:20:36 +0800

    Chapter 5: Type Conversion and Enum Type

---
+++ Chapter05/Ch05Ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace Ch05Ex02
+{
+    enum Orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte directionByte;
+            string directionString;
+            Orientation myDirection = Orientation.north;
+            WriteLine($"myDirection = {myDirection}");
+
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            WriteLine($"byte equivalent = {directionByte}");
+            WriteLine($"string equivalent = {directionString}");
+            ReadKey();
+        }
+    }
+}
