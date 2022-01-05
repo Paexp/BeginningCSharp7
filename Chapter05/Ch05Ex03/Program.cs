95d09f445d06b57c4c2f8735138bf1c2c0a48b9f
Paexp <1145009208@qq.com>
2022-01-05 20:17:25 +0800

    Chapter 5: More about Variables

---
+++ Chapter05/Ch05Ex03/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using static System.Convert;
+
+namespace Ch05Ex03;
+
+enum Orientation : byte
+{
+    north = 1,
+    south = 2,
+    east = 3,
+    west = 4
+}
+
+internal struct Route
+{
+    public Orientation direction;
+    public double distance;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Route myRoute;
+        double myDistance;
+        WriteLine("1) North\n2) South\n3) East\n4) West");
+        int myDirection;
+        do
+        {
+            WriteLine("Select a direction:");
+            myDirection = ToInt32(ReadLine());
+        }
+        while (myDirection < 1 || myDirection > 4);
+        WriteLine("Input a distance:");
+        myDistance = ToDouble(ReadLine());
+        myRoute.direction = (Orientation)myDirection;
+        myRoute.distance = myDistance;
+        WriteLine($"myRoute specifies a direction of {myRoute.direction} " + $"and a distance of {myRoute.distance}");
+        ReadKey();
+    }
+}
