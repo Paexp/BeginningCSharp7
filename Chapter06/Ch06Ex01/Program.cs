4227659db3933371e7740e383bc52e505c410f93
Paexp <1145009208@qq.com>
2022-01-06 21:54:09 +0800

    Chapter 6: Functions

---
+++ Chapter06/Ch06Ex01/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+static void Write()
+{
+    WriteLine("Text output from function.");
+}
+
+Write();
+ReadKey();
---
+++ Chapter06/Ch06Ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+static int SumVals(params int[] vals)
+{
+    int sum = 0;
+    foreach (int val in vals)
+    {
+        sum += val;
+    }
+    return sum;
+}
+
+int sum = SumVals(1, 5, 2, 9, 8);
+WriteLine($"Summed Values = {sum}");
+ReadKey();
