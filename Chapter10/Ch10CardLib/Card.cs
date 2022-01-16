003c6f1524e1a67b9acd59fbf3afa138ebef9c80
Paexp <1145009208@qq.com>
2022-01-16 15:55:10 +0800

    Chapter 10: Defining Class Members

---
+++ Chapter10/Ch10CardLib/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Ch10CardLib
+{
+    public class Card
+    {
+        public readonly Rank rank;
+        public readonly Suit suit;
+
+        private Card() {}
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString() => "The " + rank + " of " + suit + "s";
+    }
+}
---
+++ Chapter10/Ch10Ex01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ch10Ex01;
+using static System.Console;
+
+WriteLine("Creating object myObject ...");
+MyClass myObj = new MyClass("My Object");
+WriteLine("MyObj created.");
+for (int i = -1; i <= 0; i++)
+{
+    try
+    {
+        WriteLine($"\nAttempting to assign {i} to myObj.Val...");
+        myObj.Val = i;
+        WriteLine($"Value {myObj.Val} assigned to myObj.Val.");
+    }
+    catch (Exception e)
+    {
+        WriteLine($"Exception {e.GetType().FullName} thrown.");
+        WriteLine($"Message:\n\"{e.Message}\"");
+    }
+}
+WriteLine("\nOutputting myObj.ToString()...");
+WriteLine(myObj.ToString());
+WriteLine("myObj.ToString() Output.");
+WriteLine("\nmyDoubledIntProp = 5...");
+WriteLine($"Getting myDoubledIntProp of 5 is {myObj.myDoubledIntProp}");
+ReadKey();
