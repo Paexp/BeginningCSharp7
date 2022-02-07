d54c5f6a8d278b61ac600e27cde0202d305c5e52
Paexp <1145009208@qq.com>
2022-02-07 22:11:28 +0800

    Chapter 11: Collections

---
+++ Chapter11/Ch11CardLib/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace Ch11CardLib
+{
+    public class Card : ICloneable
+    {
+        public readonly Rank rank;
+        public readonly Suit suit;
+
+        private Card() {}
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString() => "The " + rank + " of " + suit + "s";
+
+        public object Clone() => MemberwiseClone();
+    }
+}
---
+++ Chapter11/Ch11Ex03/Program.cs
@@ -0,0 +1,6 @@
+﻿using Ch11Ex03;
+
+Primes primesFrom2To1000 = new Primes(2, 1000);
+foreach ( long i in primesFrom2To1000 )
+    Console.Write($"{i} ");
+Console.ReadKey();
