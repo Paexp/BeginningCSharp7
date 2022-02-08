26de19aa85e7ef4143e17d4f0d438dec8526454c
Paexp <1145009208@qq.com>
2022-02-08 20:46:25 +0800

    Chapter 11: Collections, Comparisons and Conversions

---
+++ Chapter11/Ch11CardClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ch11CardLib;
+using static System.Console;
+
+Deck myDeck = new Deck();
+myDeck.Shuffle();
+for (int i = 0; i < 52; i++)
+{
+    Card tempCard = myDeck.GetCard(i);
+    Write(tempCard.ToString());
+    if (i != 51)
+        Write(", ");
+    else
+        WriteLine();
+}
+ReadKey();
---
+++ Chapter11/Ch11Ex05/Program.cs
@@ -0,0 +1,31 @@
+﻿using Ch11Ex05;
+using System.Collections;
+using static System.Console;
+
+ArrayList list = new ArrayList();
+list.Add(new Person("Rual", 30));
+list.Add(new Person("Donna", 25));
+list.Add(new Person("Mary", 27));
+list.Add(new Person("Ben", 44));
+WriteLine("Unsorted people:");
+for (int i = 0; i < list.Count; i++)
+{
+    WriteLine($"{(list[i] as Person).Name } ({(list[i] as Person).Age })");
+}
+WriteLine();
+WriteLine(
+   "People sorted with default comparer (by age):");
+list.Sort();
+for (int i = 0; i < list.Count; i++)
+{
+    WriteLine($"{(list[i] as Person).Name } ({(list[i] as Person).Age })");
+}
+WriteLine();
+WriteLine(
+   "People sorted with nondefault comparer (by name):");
+list.Sort(PersonComparerName.Default);
+for (int i = 0; i < list.Count; i++)
+{
+    WriteLine($"{(list[i] as Person).Name } ({(list[i] as Person).Age })");
+}
+ReadKey();
