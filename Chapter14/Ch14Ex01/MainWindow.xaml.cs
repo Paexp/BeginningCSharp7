f64fd12a42a14fad7debcd23ae323ac5984a6536
Paexp <1145009208@qq.com>
2022-02-09 00:16:30 +0800

    Chapter 14: About Window Of KarliCards

---
+++ Chapter14/Ch14Ex01/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Ch14Ex01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rotatedButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, bubbling up");
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, bubbling up");
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, bubbling up");
+        }
+
+        private void rotatedButton_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, tunneling down");
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, tunneling down");
+            e.Handled = true;
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, tunneling down");
+        }
+    }
+}
